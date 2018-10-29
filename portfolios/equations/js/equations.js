1aade9db81d5861aed2edefde9cb5559229fc993
Hüseyin <yigit.hsyn@gmail.com>
2018-10-29 22:30:55 +0300

    database update with calc module revision

--- portfolios/equations/js/equations.js
+++ portfolios/equations/js/equations.js
@@ -1,21 +1,46 @@
 var equations = [
   {
+    category: "propagation, antenna",
+    title: "Far field condition of the antenna/aperture",
+    formula: "R_{ff} = \\dfrac{2D^2}{\\lambda} = \\dfrac{2D^2f}{c_0} \\times 10^6",
+    consts: [
+      { id: "c0", sym: "c_0", def: "\\textit{Light speed [m \\negthickspace /\\negthickspace\\negthickspace\\negthickspace s]}" },
+    ],
+    iargs: [
+      { id: "lambda", sym: "\\lambda", def: "\\textit{Free space wavelength [m]}" },
+    ],
+    args: [
+      { id: "D", sym: "D", def: "\\textit{Antenna cross-section aperture [m]}" },
+      { id: "f", sym: "f", def: "\\textit{Frequency [MHz]}" },
+    ],
+    yields: [
+      { id: "Rff", sym: "R_{ff}", def: "\\textit{Far field starting range [m]}", format: function (number) { return number.toFixed(3) } },
+      { id: "Rffkm", sym: "R_{ff}'", def: "\\textit{Far field starting range [km]}", format: function (number) { return number.toFixed(3) } },
+    ],
+    calc: {
+      labelWidth: "35px",
+      vars: { c0: 299792458, f: 300, D: 1 },
+      expr: ["2*D^2*f*1E6/c0", "2*D^2*f*1E6/(c0*10^3)"]
+    }
+  },
+  {
     category: "conversion",
     title: "Frequency and wavelength",
     definition: "Free space frequency and wavelength",
     formula: " \\begin{aligned} f &= c_0/(\\lambda\\times10^6) \\quad [MHz] \\\\ \\lambda &= c_0/(f\\times10^6) \\quad [m] \\end{aligned}",
-    args: [],
-    yields: [],
+    consts: [
+      { id: "c0", sym: "c_0", def: "\\textit{Light speed [m \\negthickspace /\\negthickspace\\negthickspace\\negthickspace s]}" },
+    ],
     exchanges: [
       { id: "f", sym: "f", def: "\\textit{Frequency [MHz]}", format: function (number) { return number.toFixed(0) } },
       { id: "lambda", sym: "\\lambda", def: "\\textit{Wavelength [m]}", format: function (number) { return number.toFixed(3) } },
     ],
     calc: {
       labelWidth: "35px",
-      vars: { f: 300, lambda: 0.010 },
+      vars: { c0: 299792458, f: 300, lambda: 0.010 },
       expr: [
-        ["f", "299792458/(f*10^6)"],
-        ["299792458/(lambda*10^6)", "lambda"],
+        ["f", "c0/(f*10^6)"],
+        ["c0/(lambda*10^6)", "lambda"],
       ]
     }
   },
@@ -23,8 +48,6 @@ var equations = [
     category: "conversion",
     title: "Inches and millimeter",
     formula: " \\begin{aligned}1 \\thickspace Inch &= 25.4 \\thickspace millimeters \\\\ 1 \\thickspace millimeter &= 0.0393700787 \\thickspace inches \\end{aligned}",
-    args: [],
-    yields: [],
     exchanges: [
       { id: "inch", sym: "inch", def: "\\textit{Inches}", format: function (number) { return number.toFixed(6) } },
       { id: "mm", sym: "mm", def: "\\textit{Millimeters}", format: function (number) { return number.toFixed(6) } },
@@ -43,8 +66,6 @@ var equations = [
     title: "Lineer, dB and dBm",
     definition: "Lineer to/from decibel conversion for power",
     formula: " \\begin{aligned}P_{dBm} &= P_{dB} + 30 = 10\\log P + 30\\\\P_{dB} &= P_{dBm} - 30 = 10\\log P \\\\ P &= 10^{P_{dB}/10} = 10^{(P_{dBm}-30)/10} \\end{aligned}",
-    args: [],
-    yields: [],
     exchanges: [
       { id: "PdBm", sym: "P_{dBm}", def: "\\textit{Decibels respect to 1 milliwatt [dBm]}", format: function (number) { return number.toFixed(2) } },
       { id: "PdB", sym: "P_{dB}", def: "\\textit{Decibels respect to 1 watt [dB]}", format: function (number) { return number.toFixed(2) } },
@@ -60,27 +81,26 @@ var equations = [
       ]
     }
   },
-  {
-    category: "antenna",
-    title: "Antenna gain for omnidirectional antennas",
-    definition: "Approximate gain for butterfly or omnidricational pattern",
-    formula: "G = \\dfrac{2}{\\cos\\Theta_1-\\cos\\Theta_2}",
-    ref: { name: "Thomas A. Milligan, Modern Antenna Design, 2nd Ed., Chapter 3, Page 13" },
-    args: [
-      { id: "theta1", sym: "\\Theta_1", def: "\\textit{Beam width in first principal plane [deg]}" },
-      { id: "theta2", sym: "\\Theta_2", def: "\\textit{Beam width in second principal plane [deg]}" },
-    ],
-    yields: [
-      { id: "G", sym: "G", def: "\\textit{Antenna gain []}", format: function (number) { return number.toFixed(2) } },
-      { id: "G", sym: "G_{dB}", def: "\\textit{Antenna gain [dB]}", format: function (number) { return number.toFixed(1) } },
-    ],
-    exchanges: [],
-    calc: {
-      labelWidth: "40px",
-      vars: { theta1: 35, theta2: 75 },
-      expr: ["2/(cos(unit(theta1,'deg')) - cos(unit(theta2,'deg')))", "10*log10(2/(cos(unit(theta1,'deg')) - cos(unit(theta2,'deg'))))"]
-    }
-  },
+  // {
+  //   category: "antenna",
+  //   title: "Antenna gain for omnidirectional antennas",
+  //   definition: "Approximate gain for butterfly or omnidricational pattern",
+  //   formula: "G = \\dfrac{2}{\\cos\\Theta_1-\\cos\\Theta_2}",
+  //   ref: { name: "Thomas A. Milligan, Modern Antenna Design, 2nd Ed., Chapter 3, Page 13" },
+  //   args: [
+  //     { id: "theta1", sym: "\\Theta_1", def: "\\textit{Beam width in first principal plane [deg]}" },
+  //     { id: "theta2", sym: "\\Theta_2", def: "\\textit{Beam width in second principal plane [deg]}" },
+  //   ],
+  //   yields: [
+  //     { id: "G", sym: "G", def: "\\textit{Antenna gain []}", format: function (number) { return number.toFixed(2) } },
+  //     { id: "G", sym: "G_{dB}", def: "\\textit{Antenna gain [dB]}", format: function (number) { return number.toFixed(1) } },
+  //   ],
+  //   calc: {
+  //     labelWidth: "40px",
+  //     vars: { theta1: 35, theta2: 75 },
+  //     expr: ["2/(cos(unit(theta1,'deg')) - cos(unit(theta2,'deg')))", "10*log10(2/(cos(unit(theta1,'deg')) - cos(unit(theta2,'deg'))))"]
+  //   }
+  // },
   {
     category: "antenna",
     title: "Antenna gain for pencil beam antennas",
@@ -88,14 +108,13 @@ var equations = [
     formula: "G = \\dfrac{41.253}{\\Theta_1\\Theta_2}",
     ref: { name: "John D. Kraus, Antennas for All Applications, 3rd Ed., Page 24" },
     args: [
-      { id: "theta1", sym: "\\Theta_1", def: "\\textit{Beam width in first principal plane [deg]}" },
-      { id: "theta2", sym: "\\Theta_2", def: "\\textit{Beam width in second principal plane [deg]}" },
+      { id: "theta1", sym: "\\Theta_1", def: "\\textit{Beam width in 1st principal plane [deg]}" },
+      { id: "theta2", sym: "\\Theta_2", def: "\\textit{Beam width in 2nd principal plane [deg]}" },
     ],
     yields: [
       { id: "G", sym: "G", def: "\\textit{Antenna gain []}", format: function (number) { return number.toFixed(1) } },
       { id: "G", sym: "G_{dB}", def: "\\textit{Antenna gain [dB]}", format: function (number) { return number.toFixed(0) } },
     ],
-    exchanges: [],
     calc: {
       labelWidth: "40px",
       vars: { theta1: 20, theta2: 20 },
@@ -117,7 +136,6 @@ var equations = [
     yields: [
       { id: "PL", sym: "PL", def: "\\textit{Path loss [dB]}", format: function (number) { return number.toFixed(1) } }
     ],
-    exchanges: [],
     calc: {
       labelWidth: "35px",
       vars: { f: 2200, R: 50, G1: 25, G2: 20 },
@@ -138,7 +156,6 @@ var equations = [
     yields: [
       { id: "G", sym: "G", def: "\\textit{Gain [dB]}", format: function (number) { return number.toFixed(1) } },
     ],
-    exchanges: [],
     calc: {
       labelWidth: "30px",
       vars: { k: 0.55, D: 5, lambda: 0.6125 },
@@ -159,7 +176,6 @@ var equations = [
       { id: "S", sym: "S", def: "\\textit{Power density } \\it{[W\\negthickspace /\\enspace m^2]}", format: function (number) { return number.toExponential(1) } },
       { id: "SdB", sym: "S_{dB}", def: "\\textit{Power density } \\it{[dBW\\negthickspace /\\enspace m^2]}", format: function (number) { return number.toFixed(1) } }
     ],
-    exchanges: [],
     calc: {
       labelWidth: "45px",
       vars: { Po: 3, G: 31.62, R: 1E6 },
@@ -176,7 +192,5 @@ var equations = [
       { id: "Po", sym: "P_o", def: "\\textit{Input power [W]}" },
       { id: "G", sym: "G", def: "\\textit{Antenna gain []}" },
     ],
-    yields: [],
-    exchanges: [],
   },
 ]
