e80f7e619685d0500a56ba3754a4968225bfb301
Hüseyin <yigit.hsyn@gmail.com>
2018-11-08 22:42:48 +0300

    new template version

---
+++ portfolios/industry/js/modal.js
@@ -0,0 +1,9 @@
+ready(() => {
+  $('.modal').on('shown.bs.modal', function (e) {
+    var el = document.querySelector("form");
+    while (el.firstChild) el.removeChild(el.firstChild);
+    var eventDom = e.relatedTarget.text
+    if (eventDom == "Feedback") feedback();
+    else calculate(e);
+  })
+})
