f552945525b6f28d4095753b836f01556cf22e52
Hüseyin <yigit.hsyn@gmail.com>
2019-01-01 14:21:43 +0300

    Portoflios : Books : addItem functionality tested.

--- portfolios/books/js/add.js
+++ portfolios/books/js/add.js
@@ -8,11 +8,15 @@ webix.ready(function () {
   })
 })
 
+var newItem = {}
+
 // Add Item form
 var addFormItems = [
   { id: "addItemTitle", name: "title", view: "text", label: "Title" },
   { id: "addItemAuthor", name: "author", view: "text", label: "Author(s)" },
   { id: "addItemPublisher", name: "publisher", view: "text", label: "Publisher" },
+  { id: "addItemPublisherSite", name: "publisherSite", view: "text", label: "Publisher site" },
+  { id: "addItemAmazonSite", name: "amazonSite", view: "text", label: "Amazon site" },
   {
     cols: [
       { id: "addItemEdition", name: "edition", view: "text", label: "Edition:" },
@@ -23,7 +27,12 @@ var addFormItems = [
   {
     cols: [
       { id: "addItemFile", name: "filename", view: "text", label: "File:", gravity: 2 },
-      { id: "addItemUploadFile", view: "uploader", autosend: false, label: "Select file", graivty: 1, accept: "application/pdf" },
+      {
+        id: "addItemFileUpload", view: "uploader", autosend: false, label: "Select file", graivty: 1, accept: "application/pdf",
+        multiple: false,
+        upload: "/filesystem/api/files",
+        formData: { folder: "portfolios/books" },
+      },
     ], margin: 25
   },
 ]
@@ -35,7 +44,7 @@ webix.ui({
   headHeight: 0,
   modal: true,
   width: window.innerWidth - 400,
-  height: window.innerHeight - 200,
+  height: window.innerHeight - 100,
   body: {
     rows: [
       {
@@ -44,18 +53,23 @@ webix.ui({
             rows: [
               { id: "coverImage", css: "text-center", template: '<img id="coverImage" class="text-center mw-100 mh-100 align-self-center" src="https://uppy.io/images/logos/uppy-dog-head-arrow.svg">' },
               {
-                id: "coverImageUploader",
+                id: "addItemCoverUpload",
                 view: "uploader",
                 value: "Cover image",
                 name: "files",
                 accept: "image/png, image/gif, image/jpeg",
+                multiple: false,
                 autosend: false,
-                upload: "/files",
+                upload: "/filesystem/api/files",
+                formData: { folder: "portfolios/books/covers" },
               }],
             graivty: 1
           },
           {
-            rows: [{}, { id: "addItemChapters", view: "button", label: "Edit chapters" }],
+            rows: [{},
+            { id: "addItemProceed", view: "button", label: "Add", type: "form", width: 100, height: 50 },
+            { id: "addItemCancel", view: "button", label: "Cancel", type: "danger", width: 100, height: 50 },
+            {}, { id: "addItemChapters", view: "button", label: "Edit chapters" }],
             graivty: 1
           }
         ]
@@ -67,7 +81,7 @@ webix.ui({
 
 // Chapter TreeTable
 webix.ui({
-  id: "addItemChapterTree",
+  id: "addItemChaptersWindow",
   view: "window",
   position: "center",
   headHeight: 0,
@@ -88,6 +102,13 @@ webix.ui({
         scrollX: false,
         editable: true,
         editaction: "dblclick"
+      },
+      {
+        cols: [
+          { id: "addItemChaptersWindowClose", view: "button", type: "danger", label: "Close" },
+          { id: "addItemChaptersWindowSave", view: "button", type: "form", label: "Save" },
+          { id: "addItemChaptersWindowClear", view: "button", type: "base", label: "Clear" },
+        ]
       }
     ]
   }
--- portfolios/books/js/event.js
+++ portfolios/books/js/event.js
@@ -5,7 +5,11 @@ function openAddItemWin() {
   $$("addItemWindow").show();
 }
 
-$$("coverImageUploader").attachEvent("onAfterFileAdd", function (file) {
+$$("addItemWindow").attachEvent("onShow", function () {
+  newItem = {}
+})
+
+$$("addItemCoverUpload").attachEvent("onAfterFileAdd", function (file) {
   var reader = new FileReader();
   reader.onload = function (e) {
     $('#coverImage').attr('src', e.target.result);
@@ -13,36 +17,91 @@ $$("coverImageUploader").attachEvent("onAfterFileAdd", function (file) {
   reader.readAsDataURL(document.querySelector("input").files[0]);
 })
 
-$$("addItemChapters").attachEvent("onItemClick", function () {
-  $$("addItemChapterTree").show();
+
+$$("addItemFileUpload").attachEvent("onAfterFileAdd", function (file) {
+  $$("addItemFile").setValue(file.name)
 })
 
+$$("addItemChapters").attachEvent("onItemClick", function () {
+  $$("addItemChaptersWindow").show();
+})
+
+$$("addItemCancel").attachEvent("onItemClick", function () {
+  $$("addItemDetails").clear();
+  $$("addItemWindow").hide();
+})
+
+$$("addItemProceed").attachEvent("onItemClick", function () {
+  $$("addItemFileUpload").send(function (res) {
+    if (res && res.insertedId) {
+      newItem.file = res.insertedId
+      $$("addItemCoverUpload").send(function (res) {
+        if (res && res.insertedId) {
+          newItem.cover = res.insertedId
+          newItem = Object.assign(newItem,$$("addItemDetails").getValues())
+          webix.ajax().headers({ "Content-type": "application/json" }).post("/mongodb/api/portfolios/books", newItem, function (t, d, x) {
+            if (d.json().insertedId) {
+              $$("addItemDetails").clear();
+              $$("addItemWindow").hide();
+            }
+            else {
+              console.log(d.json());
+            }
+          })
+        }
+        else console.log(res)
+      })
+    }
+    else console.log(res)
+  })
+})
+
+//=============================================================================
+// add item chapter window
+//=============================================================================
 $$("chaptersTree").attachEvent("onKeyPress", function (code, e) {
   var parentId = this.getSelectedId() || 0
   if (e.code == "KeyA") {
     this.open(parentId)
     this.add({ name: "New Chapter" }, 0, parentId)
   }
+  if (e.code == "KeyD" && parentId) {
+    this.remove(parentId)
+  }
   if (e.code == "Escape") {
     this.unselect()
   }
 })
 
-setTimeout(function () {
-  $$("addItemChapterTree").show();
-}, 2000)
-
-var items = []
-function treeToJson() {
-  $$("chaptersTree").find({}).forEach(function (item) {
-    if (item.$parent === 0){
-      items.push({name:item.name, start:item.start, stop:item.stop, chapters:[]})
+// only for first two two level
+function treeTableToJSON(treeTableId) {
+  var tree = []
+  $$(treeTableId).find({}).forEach(function (item) {
+    if (item.$parent === 0) {
+      tree.push(Object.assign({ children: [] }, item))
     }
-    else{
-      var parent = items.find(function(parentItem){
-        return parentItem.name == $$("chaptersTree").getItem(item.$parent).name
+    else {
+      var parent = tree.find(function (parentItem) {
+        return parentItem.name == $$(treeTableId).getItem(item.$parent).name
       })
-      parent.chapters.push({name:item.name, start:item.start, stop:item.stop})
+      parent.children.push(item)
     }
   });
+  return tree
 }
+
+$$("addItemChaptersWindowClose").attachEvent("onItemClick", function () {
+  $$("addItemChaptersWindow").hide();
+})
+
+$$("addItemChaptersWindowSave").attachEvent("onItemClick", function () {
+  newItem.chapters = treeTableToJSON("chaptersTree")
+})
+
+$$("addItemChaptersWindowClear").attachEvent("onItemClick", function () {
+  $$("chaptersTree").clearAll();
+})
+setTimeout(function () {
+  $$("addItemWindow").show();
+  // $$("addItemChaptersWindow").show();
+}, 2000)
