45a430dd15a5007bb0fcfbbdf2734771ff1fa22e
Hüseyin <yigit.hsyn@gmail.com>
2019-02-19 06:56:07 +0300

    book add attempt

---
+++ portfolios/books/js/books.js
@@ -0,0 +1,75 @@
+ready(function () {
+  $.get('/database/api/v1/portfolios/books', function (books) {
+    books.forEach(function (book) {
+      console.log(book)
+      var el = $("<a />", { class: 'grid__item' });
+      var el_h0 = $('<h2 />', { class: 'title title--preview', text: book.title })
+      var el_div0 = $('<div />', { class: 'loader' })
+      var el_span0 = $('<span />', { class: 'category', text: book.author })
+      var el_div1 = $('<div />', { class: 'meta meta--preview' })
+      var el_div1_img0 = $('<img />', { class: 'meta__avatar' })
+      var el_div1_span0 = $('<span />', { class: 'meta__date' })
+      var el_div1_span1 = $('<span />', { class: 'meta__reading-time' })
+      var el_div1_span1_i0 = $('<i />', { class: 'fa fa-clock-o' })
+
+      el.append(el_h0)
+      el.append(el_div0)
+      el.append(el_span0)
+      el.appendTo($(".grid"))
+
+    })
+  })
+
+  // let argTypes = ["consts", "iargs", "args", "exchanges"]
+  // equations.forEach(function (equation) {
+  //   var el = $("<div />", { class: equation.category.replaceAll(",", "") });
+  //   var popover = "\\begin{aligned}"
+  //   argTypes.forEach(function(argType){
+  //     if(equation[argType]){
+  //       equation[argType].forEach(function(arg){
+  //         popover += arg.sym + "&: " + arg.def + "\\\\"
+  //       })
+  //     }
+  //   })
+  //   popover += "\\end{aligned}"
+  //   var el_div0 = $("<div />", {
+  //     "data-content": katex.renderToString(popover),
+  //     html: katex.renderToString(equation.formula, { displayMode: true, throwOnError: false })
+  //   })
+  //   var el_div1 = $("<div />")
+  //   var el_div1_h0 = $("<h5 />", { html: equation.title })
+  //   if (!!equation.definition) { var el_div1_p0 = $("<p />", { html: equation.definition }) }
+  //   if (!!equation.calc) { var el_div1_button0 = $("<button />") }
+  //   if (!!equation.ref) {
+  //     var el_div1_a0 = $("<a />")
+  //     var el_div1_a0_small0 = $("<small />", { text: equation.ref.name })
+  //     if (equation.ref.url) {
+  //       el_div1_a0.attr("href", equation.ref.url)
+  //       el_div1_a0.attr("target", "_blank")
+  //       el_div1_a0.attr("rel", "noopener noreferrer")
+  //     }
+  //   }
+  //   el_div1.append(el_div1_h0)
+  //   if (!!equation.definition) el_div1.append(el_div1_p0)
+  //   if (!!equation.ref) { el_div1_a0.append(el_div1_a0_small0); el_div1.append(el_div1_a0); }
+  //   if (!!equation.calc) el_div1.append(el_div1_button0)
+  //   el.append(el_div0)
+  //   el.append(el_div1)
+  //   el.appendTo($(".card-columns"))
+  // })
+  // $(".card-columns > div").addClass("card mx-1 my-1 text-center")
+  // $(".card-columns > .card > div:even").attr("tabIndex", 0)
+  // $(".card-columns > .card > div:even").attr("data-toggle", "popover")
+  // $(".card-columns > .card > div:odd").addClass("card-body p-2")
+  // $(".katex-display").css("margin-bottom", "8px")
+  // $(".card-columns > .card > div > h5").addClass("card-title mt-0 mb-2")
+  // $(".card-columns > .card > div > p").addClass("mb-1")
+  // $(".card-columns > .card > div > a").addClass("mt-0")
+  // $(".card-columns > .card > div > a > small").addClass("form-text")
+  // $(".card-columns > .card > div > a > small").css("font-style", "italic")
+  // $(".card-columns > .card > div > button").addClass("btn btn-secondary mt-2")
+  // $(".card-columns > .card > div > button").attr("data-toggle", "modal")
+  // $(".card-columns > .card > div > button").attr("data-target", ".modal")
+  // $(".card-columns > .card > div > button").text("Calculate")
+  // renderMath()
+})
